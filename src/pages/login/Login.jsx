9447a8357cffaddeb4529e8fe1b2ff81e3a678e4
Roman <romantimilsina@gmail.com>
2023-05-08 12:30:17 +0545

    login_page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
@@ -6,8 +6,26 @@ import "./login.scss";
 const Login = () => {
   const { login } = useContext(AuthContext);
 
-  const handleLogin = () => {
-    login();
+  const [inputs, setInputs] = useState({
+    username:"",
+    password:"",
+  })
+
+  const [err, setErr] = useState(null)
+
+  const handleChange = e => {
+    setInputs(prev => ({...prev, [e.target.name]:e.target.value}))
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try{
+      
+      await login(inputs);
+
+    }catch(err){
+      setErr(err.response.data)
+    }
   };
 
   return (
@@ -28,8 +46,9 @@ const Login = () => {
         <div className="right">
           <h1>Login</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
+            <input type="password" placeholder="Password"name="password" onChange={handleChange} />
+            {err}
             <button onClick={handleLogin}>Login</button>
           </form>
         </div>
